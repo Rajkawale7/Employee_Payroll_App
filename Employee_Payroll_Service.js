fd1ba713682664dbdd3681e9662210411fc2e19f
Rajkawale7 <Rajkawale712@gmail.com>
2022-06-06 21:48:29 +0530

    Added: Program to set Event Listener on Documents

--- Employee_Payroll_Service.js
+++ Employee_Payroll_Service.js
@@ -135,3 +135,29 @@ function save() {
         console.error(e);
     }
 }
+
+//Day 44 : UC2:: On document, setting Event Listeners
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    const name = document.querySelector ('#name');
+    const textError = document.querySelector ('.text-error');
+    name.addEventListener('input', function(){
+        if(name.value.length == 0){
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e){
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    });
+});
